6f3fb6c0b9b88be7e5b1ec163d29c2d637d32075
Michael Centrelli <Centm860@newschool.edu>
2017-09-13 11:30:42 -0600

    Add Distance From Zip to View

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,7 @@ class SearchController < ApplicationController
   def index
     @stores = Store.store_info(params[:zip]) 
     @store_total = Store.store_total(params[:zip])
+    @zip = params[:zip]
   end
   
 end
