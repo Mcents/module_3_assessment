e042177fe5759ac64b4022444df6e14dc2121e78
Michael Centrelli <Centm860@newschool.edu>
2017-09-13 10:03:59 -0600

    Add Method for Retrieving Best Buy Stores

--- app/services/best_service.rb
+++ app/services/best_service.rb
@@ -1,5 +1,19 @@
 class BestService
   
   def initialize
+    @conn ||= Faraday.new(url: "https://api.bestbuy.com") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
   end
+
+  def store_info(zip)
+    store_json = @conn.get do |req|
+      req.url "v1/stores"
+      req.params['format'] = "json"
+      req.params['show'] = 'longName,city,distance,phone,storeType,storeId'
+      req.params['apiKey'] = "#{ENV['API_KEY']}"
+    end 
+    binding.pry
+  end
+
 end
