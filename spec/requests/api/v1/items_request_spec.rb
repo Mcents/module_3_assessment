0b2fc93fa96ab1a3348b08e96400e3ab154e00f9
Michael Centrelli <Centm860@newschool.edu>
2017-09-13 09:18:10 -0600

    Pass Test for Get Item request Spec

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -15,4 +15,18 @@ describe "all items endpoint" do
       expect(items.count).to eq(2)
     end
   end
+
+  context "/api/v1/items/1" do
+    it "retrieves an individual item" do
+      item = Item.create(name: "Robot1", description: "cool robot", image_url: "google.com/images")
+
+      get "/api/v1/items/#{item.id}"
+
+      items = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(items["name"]).to eq("Robot1")
+      expect(items["description"]).to eq("cool robot")
+    end
+  end
 end
